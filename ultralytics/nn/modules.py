91fd586f405a830905c244be49077e2c255da45e
az0422 <87636192+az0422@users.noreply.github.com>
2023-04-25 23:04:36 +0900

    add ResidualBlocks layer

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -54,6 +54,14 @@ class Conv(nn.Module):
         """Perform transposed convolution of 2D data."""
         return self.act(self.conv(x))
 
+class ResidualBlocks(nn.Module):
+    def __init__(self, c1, c2, n=1):
+        super().__init__()
+        self.conv1 = Conv(c2, c2, 1, 1)
+        self.conv2 = Conv(c2, c2, 3, 1)
+
+    def forward(self, x):
+        return x + self.conv2(self.conv1(x))
 
 class DWConv(Conv):
     """Depth-wise convolution."""
