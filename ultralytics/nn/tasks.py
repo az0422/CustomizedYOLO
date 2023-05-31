82172e1d61e27729762b6b54be9d8f12441fd00a
az0422 <87636192+az0422@users.noreply.github.com>
2023-05-31 15:56:14 +0900

    fix a bug

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -533,7 +533,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is InceptionBlock:
             c2 = ch[f] // args[0] * 4
             c1 = ch[f]
-            args = [c1]
+            args = [c1, args[0]]
 
         elif m in (EfficientBlock, SPPCSP, SPPFCSP, SPPFCSPF, ResidualBlocks, ResidualBlock,
                    PoolResidualBlock, PoolResidualBlocks):
