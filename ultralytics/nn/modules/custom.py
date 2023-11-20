06ab64d9415a1b2bc8af8723cfcb04c1087e75f7
az0422 <87636192+az0422@users.noreply.github.com>
2023-11-20 15:58:54 +0900

    Update Test Detector

--- ultralytics/nn/modules/custom.py
+++ ultralytics/nn/modules/custom.py
@@ -577,8 +577,9 @@ class HeaderConvTinyv2(nn.Module):
 
         conv1 = Conv(c1, c2, 1, 1)
         conv2 = LightConv(c2, c2, 3, 1)
+        conv3 = LightConv(c2, c2, 3, 1)
 
-        self.m = nn.Sequential(conv1, conv2)
+        self.m = nn.Sequential(conv1, conv2, conv3)
     
     def forward(self, x):
         return self.m(x)
