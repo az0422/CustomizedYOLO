3a87d8537ff16a983366cffb817076bbab69caa2
az0422 <87636192+az0422@users.noreply.github.com>
2023-12-28 17:55:17 +0900

    update New weights and cfg

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -561,7 +561,7 @@ def torch_safe_load(weight):
     file = None
     
     if str(weight).startswith("yolov8-mobile"):
-        file = attempt_download_asset(weight, repo="az0422/yolov8-mobile-weights", release="v0.02")
+        file = attempt_download_asset(weight, repo="az0422/yolov8-mobile-weights", release="v0.03")
     else:
         file = attempt_download_asset(weight)  # search online if missing locally
     
